4d47f1491adb527cbdfdf491e55623d39f0b28df
David Grudl <david@grudl.com>
2010-10-06 17:15:56 +0200

    method sorting by natural order or alphabetically

---
+++ templates/media/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+	$("#search input[type=text]").autocomplete(classes, {
+		matchContains: true,
+		scrollHeight: 200,
+		max: 20,
+		formatItem: function(row) { return row[0].replace(/^(.+\\)(.+)$/, '<small>$1</small>$2'); },
+		formatMatch: function(row) { return row[0]; }
+	});
+	
+	$("table.summary tr").each(function(index) {
+		do { index = '0' + index; } while (index.length < 3);
+		$(this).attr('data-orig-order', index);
+	});
+	
+	$("table.summary:has(tr[data-order]) caption").click(function() {
+		this.sorted = !this.sorted;
+		var attr = this.sorted ? 'data-order' : 'data-orig-order';
+		$(this).closest("table").find('tr').sortElements(function(a, b) {
+			return $(a).attr(attr) > $(b).attr(attr) ? 1 : -1;
+		});
+		return false;
+	}).addClass('switchable').attr('title', 'Switch between natural and alphabetical order');
+	
+});
