d6fe17439e114b643c6ac408afdff26280af52ca
David Grudl <david@grudl.com>
2010-10-07 13:47:23 +0200

    added anchor links to properties, methods and constants

--- templates/media/main.js
+++ templates/media/main.js
@@ -1,5 +1,6 @@
 $(function(){
 
+	// enables search autocompletion
 	$("#search input[type=text]").autocomplete(classes, {
 		matchContains: true,
 		scrollHeight: 200,
@@ -8,11 +9,13 @@ $(function(){
 		formatMatch: function(row) { return row[0]; }
 	});
 
+	// saves original order
 	$("table.summary:has(tr[data-order]) tr").each(function(index) {
 		do { index = '0' + index; } while (index.length < 3);
 		$(this).attr('data-orig-order', index);
 	});
 
+	// switches between natural and alphabetical order
 	$("table.summary:has(tr[data-order]) caption").click(function() {
 		this.sorted = !this.sorted;
 		var attr = this.sorted ? 'data-order' : 'data-orig-order';
@@ -22,6 +25,7 @@ $(function(){
 		return false;
 	}).addClass('switchable').attr('title', 'Switch between natural and alphabetical order');
 
+	// delayed hover efect on method summary
 	var timeout;
 	$("tr:has(.detailed)").hover(function(){
 		clearTimeout(timeout);
@@ -32,5 +36,10 @@ $(function(){
 		}, 500);
 	}, function(){
 		clearTimeout(timeout);
+
+	}).click(function(){ // immediate hover effect on method summary
+		clearTimeout(timeout);
+		$(this).find('.short').hide();
+		$(this).find('.detailed').show();
 	});
 });
