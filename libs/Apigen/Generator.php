20b785762a5cfa9463b031ca9a27edb10a70229a
Jaroslav Hanslik <kukulich@kukulich.cz>
2011-06-10 13:29:31 +0200

    Fixed descending into symlinked directories

--- libs/Apigen/Generator.php
+++ libs/Apigen/Generator.php
@@ -70,6 +70,13 @@ class Generator extends Nette\Object
 	private $functions = null;
 
 	/**
+	 * List of symlinks.
+	 *
+	 * @var array
+	 */
+	private $symlinks = array();
+
+	/**
 	 * Progressbar settings and status.
 	 *
 	 * @var array
@@ -101,10 +108,16 @@ class Generator extends Nette\Object
 	public function parse()
 	{
 		$files = array();
+
+		$flags = \RecursiveDirectoryIterator::CURRENT_AS_FILEINFO | \RecursiveDirectoryIterator::SKIP_DOTS;
+		if (defined('\RecursiveDirectoryIterator::FOLLOW_SYMLINKS')) {
+			// Available from PHP 5.3.1
+			$flags |= \RecursiveDirectoryIterator::FOLLOW_SYMLINKS;
+		}
 		foreach ($this->config->source as $source) {
 			$entries = array();
 			if (is_dir($source)) {
-				foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($source)) as $entry) {
+				foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($source, $flags)) as $entry) {
 					if (!$entry->isFile()) {
 						continue;
 					}
@@ -125,6 +138,9 @@ class Generator extends Nette\Object
 				}
 
 				$files[$entry->getPathName()] = $entry->getSize();
+				if ($entry->getPathName() !== $entry->getRealPath()) {
+					$this->symlinks[$entry->getRealPath()] = $entry->getPathName();
+				}
 			}
 		}
 
@@ -945,16 +961,18 @@ class Generator extends Nette\Object
 			foreach ($$type as $element) {
 				if ($element->isTokenized()) {
 					$template->fileName = null;
-					$file = $element->getFileName();
+					$fileName = $element->getFileName();
+					if (isset($this->symlinks[$fileName])) {
+						$fileName = $this->symlinks[$fileName];
+					}
 					foreach ($this->config->source as $source) {
-						if (0 === strpos($file, $source)) {
-							$template->fileName = is_dir($source) ? str_replace('\\', '/', substr($file, strlen($source) + 1)) : basename($file);
+						if (0 === strpos($fileName, $source)) {
+							$template->fileName = is_dir($source) ? str_replace('\\', '/', substr($fileName, strlen($source) + 1)) : basename($fileName);
 							break;
 						}
 					}
 					if (null === $template->fileName) {
-						$template->fileName = $element->getFileName();
-						//throw new Exception(sprintf('Could not determine element %s relative path', $element->getName()));
+						throw new Exception(sprintf('Could not determine element %s relative path', $element->getName()));
 					}
 				}
 
