f17453feb405844d673fd15ab2b310ec2456a8bf
Jaroslav Hanslik <kukulich@kukulich.cz>
2011-07-17 21:42:38 +0200

    Version bump

--- ApiGen/ReflectionFunction.php
+++ ApiGen/ReflectionFunction.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * ApiGen 2.0 - API documentation generator.
+ * ApiGen 2.0.1 - API documentation generator.
  *
  * Copyright (c) 2010 David Grudl (http://davidgrudl.com)
  * Copyright (c) 2011 Ondřej Nešpor (http://andrewsville.cz)
