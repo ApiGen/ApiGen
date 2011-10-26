0504c4f46b9c514083e27c52110c586859017f31
Jaroslav Hanslik <kukulich@kukulich.cz>
2011-10-26 20:37:08 +0200

    Version bump

--- ApiGen/Tree.php
+++ ApiGen/Tree.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ApiGen 2.2.0 - API documentation generator for PHP 5.3+
+ * ApiGen 2.2.1 - API documentation generator for PHP 5.3+
  *
  * Copyright (c) 2010 David Grudl (http://davidgrudl.com)
  * Copyright (c) 2011 Jaroslav Hanslík (https://github.com/kukulich)
