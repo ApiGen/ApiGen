d6e61bc5a088bdcb5863c92d7cd0058b25c7743c
Jaroslav Hanslik <kukulich@kukulich.cz>
2012-11-04 12:42:06 +0100

    Cleanup

--- ApiGen/ReflectionClass.php
+++ ApiGen/ReflectionClass.php
@@ -29,14 +29,14 @@ class ReflectionClass extends ReflectionElement
 	 *
 	 * @var integer
 	 */
-	private static $methodAccessLevels = false;
+	private static $methodAccessLevels = null;
 
 	/**
 	 * Access level for properties.
 	 *
 	 * @var integer
 	 */
-	private static $propertyAccessLevels = false;
+	private static $propertyAccessLevels = null;
 
 	/**
 	 * Cache for list of parent classes.
@@ -113,7 +113,7 @@ class ReflectionClass extends ReflectionElement
 	{
 		parent::__construct($reflection, $generator);
 
-		if (false === self::$methodAccessLevels) {
+		if (null === self::$methodAccessLevels) {
 			self::$methodAccessLevels = 0;
 			self::$propertyAccessLevels = 0;
 
