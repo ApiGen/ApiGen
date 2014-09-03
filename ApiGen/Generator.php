8a128d95f4dff34bf0e8bd3db7ab2bdc87b469aa
Tomas Votruba <info@tomasvotruba.cz>
2014-09-03 14:16:24 +0200

    license in files updated

--- ApiGen/Generator.php
+++ ApiGen/Generator.php
@@ -1,14 +1,10 @@
 <?php
 
 /**
- * ApiGen 2.8.0 - API documentation generator for PHP 5.3+
- *
- * Copyright (c) 2010-2011 David Grudl (http://davidgrudl.com)
- * Copyright (c) 2011-2012 Jaroslav Hanslík (https://github.com/kukulich)
- * Copyright (c) 2011-2012 Ondřej Nešpor (https://github.com/Andrewsville)
+ * This file is part of the ApiGen (http://apigen.org)
  *
  * For the full copyright and license information, please view
- * the file LICENSE.md that was distributed with this source code.
+ * the file license.md that was distributed with this source code.
  */
 
 namespace ApiGen;
