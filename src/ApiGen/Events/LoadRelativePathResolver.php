552be6d260faca240c913d476c36b2d4ba7541b1
Tomas Votruba <info@tomasvotruba.cz>
2014-09-22 10:23:51 +0200

    resolvers events added [#345]

---
+++ src/ApiGen/Events/LoadRelativePathResolver.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the ApiGen (http://apigen.org)
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+namespace ApiGen\Events;
+
+use ApiGen\Console\ProgressBar;
+use ApiGen\Generator\Generator;
+use ApiGen\Generator\HtmlGenerator;
+use ApiGen\Generator\Resolvers\ElementResolver;
+use ApiGen\Generator\Resolvers\RelativePathResolver;
+use Nette;
+use Kdyby\Events\Subscriber;
+
+
+class LoadRelativePathResolver extends Nette\Object implements Subscriber
+{
+
+	/**
+	 * @var RelativePathResolver
+	 */
+	private $relativePathResolver;
+
+
+	public function __construct(RelativePathResolver $relativePathResolver)
+	{
+		$this->relativePathResolver = $relativePathResolver;
+	}
+
+
+	/**
+	 * @return array
+	 */
+	public function getSubscribedEvents()
+	{
+		return array(
+			'ApiGen\Generator\HtmlGenerator::onScanFinish' => 'onFinish',
+		);
+	}
+
+
+	public function onFinish(HtmlGenerator $generator)
+	{
+		$this->relativePathResolver->setSymlinks($generator->getSymlinks());
+	}
+
+}
