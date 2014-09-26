0d569ddf2c84703aba31cb87dbaf72598e4a00a1
Tomas Votruba <info@tomasvotruba.cz>
2014-09-26 20:47:27 +0200

    unused ns removed [#326]

--- src/ApiGen/Command/GenerateCommand.php
+++ src/ApiGen/Command/GenerateCommand.php
@@ -20,8 +20,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use TokenReflection\Exception\FileProcessingException;
-use TokenReflection\Exception\ParseException;
 use Tracy\Debugger;
 
 
