ab1c0798e83cd17fcf4d095139c34284f95206a8
Tomas Votruba <info@tomasvotruba.cz>
2014-10-07 01:43:39 +0200

    config: --working-dir added

--- src/ApiGen/Console/Application.php
+++ src/ApiGen/Console/Application.php
@@ -11,6 +11,9 @@ namespace ApiGen\Console;
 
 use ApiGen\ApiGen;
 use Kdyby;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 
 class Application extends Kdyby\Console\Application
@@ -33,5 +36,50 @@ class Application extends Kdyby\Console\Application
 		return parent::getLongVersion() . ' ' . ApiGen::RELEASE_DATE;
 	}
 
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function doRun(InputInterface $input, OutputInterface $output)
+	{
+		// Switch working dir
+		if ($newWorkDir = $this->getNewWorkingDir($input)) {
+			$oldWorkingDir = getcwd();
+			chdir($newWorkDir);
+		}
+
+		$result = parent::doRun($input, $output);
+
+		if (isset($oldWorkingDir)) {
+			chdir($oldWorkingDir);
+		}
+
+		return $result;
+	}
+
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function getDefaultInputDefinition()
+	{
+		$definition = parent::getDefaultInputDefinition();
+		$definition->addOption(new InputOption('--working-dir', '-d', InputOption::VALUE_REQUIRED, 'If specified, use the given directory as working directory.'));
+		return $definition;
+	}
+
+
+	/**
+	 * @return string
+	 * @throws \RuntimeException
+	 */
+	private function getNewWorkingDir(InputInterface $input)
+	{
+		$workingDir = $input->getParameterOption(array('--working-dir', '-d'));
+		if ($workingDir !== FALSE && ! is_dir($workingDir)) {
+			throw new \RuntimeException('Invalid working directory specified.');
+		}
+		return $workingDir;
+	}
 
 }
