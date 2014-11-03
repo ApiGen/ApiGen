2e72adb27ee93b3dcb1a9931dc4da03b99a8321b
Tomas Votruba <info@tomasvotruba.cz>
2014-11-03 11:43:06 +0100

    Templating: TemplateFactory now sets base variables using Elements

--- src/ApiGen/Templating/TemplateFactory.php
+++ src/ApiGen/Templating/TemplateFactory.php
@@ -10,21 +10,20 @@
 namespace ApiGen\Templating;
 
 use ApiGen\Configuration\Configuration;
+use ApiGen\Configuration\TemplateConfiguration;
+use ApiGen\Elements\AutocompleteElements;
+use ApiGen\Elements\Elements;
+use ApiGen\Elements\ElementStorage;
+use ApiGen\FileSystem\Zip;
+use ApiGen\Reflection\ReflectionElement;
 use Latte;
 use Nette;
 use Nette\Utils\ArrayHash;
+use stdClass;
 
 
-/**
- * @method TemplateFactory setConfig(array $config)
- */
 class TemplateFactory extends Nette\Object
 {
-
-	/**
-	 * @var array
-	 */
-	private $config;
 
 	/**
 	 * @var Latte\Engine
@@ -32,28 +31,155 @@ class TemplateFactory extends Nette\Object
 	private $latteEngine;
 
 	/**
+	 * @var TemplateNavigator
+	 */
+	private $templateNavigator;
+
+	/**
 	 * @var Configuration
 	 */
 	private $configuration;
 
+	/**
+	 * @var ElementStorage
+	 */
+	private $elementStorage;
 
-	public function __construct(Latte\Engine $latteEngine, Configuration $configuration)
-	{
+	/**
+	 * @var Zip
+	 */
+	private $zip;
+
+	/**
+	 * @var AutocompleteElements
+	 */
+	private $autocompleteElements;
+
+
+	public function __construct(
+		Latte\Engine $latteEngine,
+		TemplateNavigator $templateNavigator,
+		Configuration $configuration,
+		ElementStorage $elementStorage,
+		Zip $zip,
+		AutocompleteElements $autocompleteElements
+	) {
 		$this->latteEngine = $latteEngine;
+		$this->templateNavigator = $templateNavigator;
 		$this->configuration = $configuration;
+		$this->elementStorage = $elementStorage;
+		$this->zip = $zip;
+		$this->autocompleteElements = $autocompleteElements;
+	}
+
+
+//	/**
+//	 * @return Template|stdClass
+//	 */
+//	public function create()
+//	{
+//		return $this->buildTemplate();
+//	}
+
+
+	/**
+	 * @param string $name
+	 * @return Template|stdClass
+	 */
+	public function create($name = NULL)
+	{
+		$template = $this->buildTemplate();
+		if ($name) {
+			$template->setFile($this->templateNavigator->getTemplatePath($name));
+			$template->setSavePath($this->templateNavigator->getTemplateFileName($name));
+		}
+		return $template;
 	}
 
 
 	/**
-	 * @return Template|\stdClass
+	 * @param string $name
+	 * @param ReflectionElement|string $element
+	 * @return Template|stdClass
 	 */
-	public function create()
+	public function createNamedForElement($name, $element)
 	{
-		/** @var Template|\stdClass $template */
-		$template = new Template($this->latteEngine);
-		$template->config = ArrayHash::from($this->config);
-		$template->basePath = dirname($this->config['templateConfig']);
+		$template = $this->buildTemplate();
+		$template->setFile($this->templateNavigator->getTemplatePath($name));
+
+		if ($name === 'source') {
+			$template->setSavePath($this->templateNavigator->getTemplatePathForSourceElement($element));
+
+ 		} elseif ($name === 'class') {
+			$template->setSavePath($this->templateNavigator->getTemplatePathForClass($element));
+
+ 		} elseif ($name === 'constant') {
+			$template->setSavePath($this->templateNavigator->getTemplatePathForConstant($element));
+
+ 		} elseif ($name === 'function') {
+			$template->setSavePath($this->templateNavigator->getTemplatePathForFunction($element));
+		}
+
 		return $template;
 	}
 
+
+	/**
+	 * @return Template|stdClass
+	 */
+	private function buildTemplate()
+	{
+		/** @var Template|stdClass $template */
+		$template = new Template($this->latteEngine);
+		$template->config = ArrayHash::from($this->configuration->getOptions());
+		$options = $this->configuration->getOptions();
+		$template->basePath = $options['template']['templatesPath'];
+
+		$template->getLatte()->addFilter('url', 'rawurlencode');
+		$template = $this->setBaseVariables($template);
+
+		return $template;
+	}
+
+
+	/**
+	 * @param Template|stdClass $template
+	 * @return Template
+	 */
+	private function setBaseVariables(Template $template)
+	{
+		$template->namespace = NULL;
+		$template->package = NULL;
+		$template->class = NULL;
+		$template->constant = NULL;
+		$template->function = NULL;
+
+		$template->namespaces = array_keys($this->elementStorage->getPackages());
+		$template->packages = array_keys($this->elementStorage->getNamespaces());
+		$template->archive = basename($this->zip->getArchivePath());
+
+		$elements = $this->elementStorage->getElements();
+		$template->classes = array_filter($elements[Elements::CLASSES], $this->getMainFilter());
+		$template->constants = array_filter($elements[Elements::CONSTANTS], $this->getMainFilter());
+		$template->exceptions = array_filter($elements[Elements::EXCEPTIONS], $this->getMainFilter());
+		$template->functions = array_filter($elements[Elements::FUNCTIONS], $this->getMainFilter());
+		$template->interfaces = array_filter($elements[Elements::INTERFACES], $this->getMainFilter());
+		$template->traits = array_filter($elements[Elements::TRAITS], $this->getMainFilter());
+
+		$template->elements = $this->autocompleteElements->getElements();
+
+		return $template;
+	}
+
+
+	/**
+	 * @return \Closure
+	 */
+	private function getMainFilter()
+	{
+		return function ($element) {
+			/** @var ReflectionElement $element */
+			return $element->isMain();
+		};
+	}
 }
