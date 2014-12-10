05c2ea0936185cdf356e18133fbc5326527d546b
Tomas Votruba <info@tomasvotruba.cz>
2014-12-10 01:43:15 +0100

    composer: autoload with PSR-4

---
+++ src/ApiGen/Configuration/Exceptions/ConfigurationException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of the ApiGen (http://apigen.org)
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+namespace ApiGen\Configuration;
+
+
+/**
+ * Thrown when an invalid configuration is detected.
+ */
+class ConfigurationException extends \RuntimeException
+{
+
+}
