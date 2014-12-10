ec4fb198a74508b2c6ce532dc7e32c3eecb9ff58
Tomas Votruba <info@tomasvotruba.cz>
2014-12-10 01:40:19 +0100

    TemplateGenerators: FunctionElementGenerator decoupled

---
+++ src/ApiGen/Generator/TemplateGenerators/FunctionElementGenerator.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This file is part of the ApiGen (http://apigen.org)
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+namespace ApiGen\Generator\TemplateGenerators;
+
+use ApiGen\Generator\StepCounter;
+use ApiGen\Generator\TemplateGenerator;
+use ApiGen\Generator\TemplateGenerators\Loaders\NamespaceAndPackageLoader;
+use ApiGen\Parser\Elements\ElementStorage;
+use ApiGen\Reflection\ReflectionFunction;
+use ApiGen\Templating\Template;
+use ApiGen\Templating\TemplateFactory;
+use Nette;
+
+
+/**
+ * @method onGenerateProgress()
+ */
+class FunctionElementGenerator extends Nette\Object implements TemplateGenerator, StepCounter
+{
+
+	/**
+	 * @var array
+	 */
+	public $onGenerateProgress = [];
+
+	/**
+	 * @var TemplateFactory
+	 */
+	private $templateFactory;
+
+	/**
+	 * @var ElementStorage
+	 */
+	private $elementStorage;
+
+	/**
+	 * @var NamespaceAndPackageLoader
+	 */
+	private $namespaceAndPackageLoader;
+
+
+	public function __construct(
+		TemplateFactory $templateFactory,
+		ElementStorage $elementStorage,
+		NamespaceAndPackageLoader $namespaceAndPackageLoader
+	)
+	{
+		$this->templateFactory = $templateFactory;
+		$this->elementStorage = $elementStorage;
+		$this->namespaceAndPackageLoader = $namespaceAndPackageLoader;
+	}
+
+
+	public function generate()
+	{
+		foreach ($this->elementStorage->getFunctions() as $name => $function) {
+			$template = $this->templateFactory->createNamedForElement(TemplateFactory::ELEMENT_FUNCTION, $function);
+			$template = $this->loadTemplateWithParameters($template, $function);
+			$template->save();
+			$this->onGenerateProgress(1);
+		}
+	}
+
+
+	/**
+	 * @return int
+	 */
+	public function getStepCount()
+	{
+		return count($this->elementStorage->getFunctions());
+	}
+
+
+	/**
+	 * @return Template
+	 */
+	private function loadTemplateWithParameters(Template $template, ReflectionFunction $function)
+	{
+		$template = $this->namespaceAndPackageLoader->loadTemplateWithElementNamespaceOrPackage($template, $function);
+		$template->setParameters([
+			'function' => $function
+		]);
+		return $template;
+	}
+
+}
