648d0b0d82fec40be3a3622d1515bb90c24f1b23
Tomas Votruba <info@tomasvotruba.cz>
2014-12-19 00:03:54 +0100

    tests: OverviewGeneratorTest added

---
+++ tests/Generator/TemplateGenerators/OverviewGeneratorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ApiGen\Tests\ApiGen\Generator\TemplateGenerators;
+
+use ApiGen\Generator\TemplateGenerators\OverviewGenerator;
+use ApiGen\Templating\Template;
+use ApiGen\Templating\TemplateFactory;
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+
+class OverviewGeneratorTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testGenerate()
+	{
+		$templateFactoryMock = $this->getTemplateFactoryMock();
+		$overviewGenerator = new OverviewGenerator($templateFactoryMock);
+		$overviewGenerator->generate();
+		$this->assertFileExists(TEMP_DIR . '/index.html');
+	}
+
+
+	/**
+	 * @return Mockery\MockInterface
+	 */
+	private function getTemplateFactoryMock()
+	{
+		$templateFactoryMock = Mockery::mock('ApiGen\Templating\TemplateFactory');
+		$templateFactoryMock->shouldReceive('createForType')->andReturn($this->getTemplateMock());
+		$templateFactoryMock->shouldReceive('save');
+		return $templateFactoryMock;
+	}
+
+
+	/**
+	 * @return Mockery\MockInterface
+	 */
+	private function getTemplateMock()
+	{
+		$templateMock = Mockery::mock('ApiGen\Templating\Template');
+		$templateMock->shouldReceive('setSavePath')->withAnyArgs(); //, TEMP_DIR . '/index.html');
+		$templateMock->shouldReceive('save')->andReturn(file_put_contents(TEMP_DIR . '/index.html', '...'));
+		return $templateMock;
+	}
+
+}
