13f1c7aaffc1c5812b62c1cffc9c5868975b50cf
Tomas Votruba <info@tomasvotruba.cz>
2014-12-21 03:03:45 +0100

    tests: FshlSourceCodeHighlighterTest added

---
+++ tests/Generator/SourceCodeHighlighter/FshlSourceCodeHighlighter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ApiGen\Tests\Generator\SourceCodeHighlighter;
+
+use ApiGen\Generator\GeneratorQueue;
+use ApiGen\Generator\SourceCodeHighlighter\FshlSourceCodeHighlighter;
+use ApiGen\Tests\MethodInvoker;
+use FSHL\Highlighter;
+use FSHL\Lexer\Php;
+use FSHL\Output\Html;
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+
+class FshlSourceCodeHighlighterTest extends PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @var FshlSourceCodeHighlighter
+	 */
+	private $fshlSourceCodeHighlighter;
+
+
+	protected function setUp()
+	{
+		$fshlHighlighter = new Highlighter(new Html);
+		$fshlHighlighter->setLexer(new Php);
+		$this->fshlSourceCodeHighlighter = new FshlSourceCodeHighlighter($fshlHighlighter);
+	}
+
+
+	public function testHighlight()
+	{
+		$this->assertSame(
+			'<span class="php-var">$a</span> = <span class="php-num">1</span>',
+			$this->fshlSourceCodeHighlighter->highlight('$a = 1')
+		);
+	}
+
+
+	public function testHighlightAndAddLineNumbers()
+	{
+		$this->assertSame(
+			'<span class="line">1: </span><span class="php-var">$a</span> = <span class="php-num">1</span>',
+			$this->fshlSourceCodeHighlighter->highlightAndAddLineNumbers('$a = 1')
+		);
+	}
+
+}
