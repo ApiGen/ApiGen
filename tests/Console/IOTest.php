2a3aab2a88037f1f323427c6be6f873be48b276d
Tomas Votruba <info@tomasvotruba.cz>
2014-12-22 03:05:53 +0100

    tests: IOTest, ProgressBarTest added

---
+++ tests/Console/IOTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ApiGen\Tests\Console;
+
+use ApiGen\Console\IO;
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+
+class IOTest extends PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @var IO
+	 */
+	private $io;
+
+
+	protected function setUp()
+	{
+		$this->io = new IO;
+	}
+
+
+	public function testSetInputGetInput()
+	{
+		$inputMock = Mockery::mock('Symfony\Component\Console\Input\InputInterface');
+		$this->io->setInput($inputMock);
+		$this->assertInstanceOf(
+			'Symfony\Component\Console\Input\InputInterface',
+			$this->io->getInput()
+		);
+	}
+
+
+	public function testSetOutputGetOutput()
+	{
+		$outputMock = Mockery::mock('Symfony\Component\Console\Output\OutputInterface');
+		$this->io->setOutput($outputMock);
+		$this->assertInstanceOf(
+			'Symfony\Component\Console\Output\OutputInterface',
+			$this->io->getOutput()
+		);
+	}
+
+}
