79bdde7fe59a22029d010c2390363127b0d40bd2
Tomas Votruba <info@tomasvotruba.cz>
2014-12-23 15:08:12 +0100

    ReflectionFactory added

---
+++ src/ApiGen/Reflection/TokenReflection/ReflectionFactory.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * This file is part of the ApiGen (http://apigen.org)
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+namespace ApiGen\Reflection\TokenReflection;
+
+use ApiGen\Configuration\Configuration;
+use ApiGen\Parser\ParserResult;
+use ApiGen\Reflection\ReflectionBase;
+use ApiGen\Reflection\ReflectionClass;
+use ApiGen\Reflection\ReflectionConstant;
+use ApiGen\Reflection\ReflectionFunction;
+use ApiGen\Reflection\ReflectionMethod;
+use ApiGen\Reflection\ReflectionMethodMagic;
+use ApiGen\Reflection\ReflectionParameter;
+use ApiGen\Reflection\ReflectionParameterMagic;
+use ApiGen\Reflection\ReflectionProperty;
+use TokenReflection\IReflectionClass;
+use TokenReflection\IReflectionConstant;
+use TokenReflection\IReflectionFunction;
+use TokenReflection\IReflectionMethod;
+use TokenReflection\IReflectionParameter;
+use TokenReflection\IReflectionProperty;
+
+
+class ReflectionFactory
+{
+
+	/**
+	 * @var Configuration
+	 */
+	private $configuration;
+
+	/**
+	 * @var ParserResult
+	 */
+	private $parserResult;
+
+
+	public function __construct(Configuration $configuration, ParserResult $parserResult)
+	{
+		$this->configuration = $configuration;
+		$this->parserResult = $parserResult;
+	}
+
+
+	/**
+	 * @param IReflectionClass|IReflectionConstant|IReflectionFunction $tokenReflection
+	 * @return ReflectionClass|ReflectionConstant|ReflectionMethod
+	 */
+	public function createFromReflection($tokenReflection)
+	{
+		$reflection = $this->createByReflectionType($tokenReflection);
+		return $this->setDependencies($reflection);
+	}
+
+
+	/**
+	 * @return ReflectionMethodMagic
+	 */
+	public function createMethodMagic()
+	{
+		$reflection = new ReflectionMethodMagic;
+		return $this->setDependencies($reflection);
+	}
+
+
+	/**
+	 * @return ReflectionParameterMagic
+	 */
+	public function createParameterMagic()
+	{
+		$reflection = new ReflectionParameterMagic;
+		return $this->setDependencies($reflection);
+	}
+
+
+	/**
+	 * @param IReflectionClass|IReflectionConstant|IReflectionMethod $reflection
+	 * @return ReflectionClass|ReflectionConstant|ReflectionMethod
+	 */
+	private function createByReflectionType($reflection)
+	{
+		$reflectionHashTable = [];
+		if ($reflection instanceof IReflectionClass) {
+			return new ReflectionClass($reflection);
+
+		} elseif ($reflection instanceof IReflectionConstant) {
+			return new ReflectionConstant($reflection);
+
+		} elseif ($reflection instanceof IReflectionMethod) {
+			return new ReflectionMethod($reflection);
+
+		} elseif ($reflection instanceof IReflectionProperty) {
+			return new ReflectionProperty($reflection);
+
+		} elseif ($reflection instanceof IReflectionParameter) {
+			return new ReflectionParameter($reflection);
+
+		} elseif ($reflection instanceof IReflectionFunction) {
+			return new ReflectionFunction($reflection);
+		}
+
+		throw new \RuntimeException('Invalid reflection class type ' . get_class($reflection));
+	}
+
+
+	/**
+	 * @return ReflectionBase
+	 */
+	private function setDependencies(ReflectionBase $reflection)
+	{
+		$reflection->setConfiguration($this->configuration);
+		$reflection->setParserResult($this->parserResult);
+		$reflection->setReflectionFactory($this);
+		return $reflection;
+	}
+
+}
---
+++ tests/Reflections/TokenReflection/ReflectionFactoryTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace ApiGen\Tests\Reflection\TokenReflection;
+
+use ApiGen\Reflection\TokenReflection\ReflectionFactory;
+use Mockery;
+use PHPUnit_Framework_Assert;
+use PHPUnit_Framework_TestCase;
+use TokenReflection\Php\ReflectionClass;
+
+
+class ReflectionFactoryTest extends PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @var ReflectionFactory
+	 */
+	private $reflectionFactory;
+
+
+	protected function setUp()
+	{
+		$configurationMock = Mockery::mock('ApiGen\Configuration\Configuration');
+		$parserResultMock = Mockery::mock('ApiGen\Parser\ParserResult');
+		$this->reflectionFactory = new ReflectionFactory($configurationMock, $parserResultMock);
+	}
+
+
+	public function testCreateMethodMagic()
+	{
+		$methodMagic = $this->reflectionFactory->createMethodMagic();
+		$this->assertInstanceOf('ApiGen\Reflection\ReflectionMethodMagic', $methodMagic);
+		$this->checkLoadedProperties($methodMagic);
+	}
+
+
+	public function testCreateParameterMagic()
+	{
+		$parameterMagic = $this->reflectionFactory->createParameterMagic();
+		$this->assertInstanceOf('ApiGen\Reflection\ReflectionParameterMagic', $parameterMagic);
+		$this->checkLoadedProperties($parameterMagic);
+	}
+
+
+	public function testCreateFromReflectionClass()
+	{
+		$tokenReflectionClassMock = Mockery::mock('TokenReflection\IReflectionClass', 'Nette\Object');
+		$reflectionClass = $this->reflectionFactory->createFromReflection($tokenReflectionClassMock);
+		$this->assertInstanceOf('ApiGen\Reflection\ReflectionClass', $reflectionClass);
+		$this->checkLoadedProperties($reflectionClass);
+	}
+
+
+	public function testCreateFromReflectionFunction()
+	{
+		$tokenReflectionFunctionMock = Mockery::mock('TokenReflection\IReflectionFunction', 'Nette\Object');
+		$reflectionFunction = $this->reflectionFactory->createFromReflection($tokenReflectionFunctionMock);
+		$this->assertInstanceOf('ApiGen\Reflection\ReflectionFunction', $reflectionFunction);
+		$this->checkLoadedProperties($reflectionFunction);
+	}
+
+
+	public function testCreateFromReflectionMethod()
+	{
+		$tokenReflectionMethodMock = Mockery::mock('TokenReflection\IReflectionMethod', 'Nette\Object');
+		$reflectionMethod = $this->reflectionFactory->createFromReflection($tokenReflectionMethodMock);
+		$this->assertInstanceOf('ApiGen\Reflection\ReflectionMethod', $reflectionMethod);
+		$this->checkLoadedProperties($reflectionMethod);
+	}
+
+
+	public function testCreateFromReflectionProperty()
+	{
+		$tokenReflectionPropertyMock = Mockery::mock('TokenReflection\IReflectionProperty', 'Nette\Object');
+		$reflectionProperty = $this->reflectionFactory->createFromReflection($tokenReflectionPropertyMock);
+		$this->assertInstanceOf('ApiGen\Reflection\ReflectionProperty', $reflectionProperty);
+		$this->checkLoadedProperties($reflectionProperty);
+	}
+
+
+	public function testCreateFromReflectionParameter()
+	{
+		$tokenReflectionParameterMock = Mockery::mock('TokenReflection\IReflectionParameter', 'Nette\Object');
+		$reflectionParameter = $this->reflectionFactory->createFromReflection($tokenReflectionParameterMock);
+		$this->assertInstanceOf('ApiGen\Reflection\ReflectionParameter', $reflectionParameter);
+		$this->checkLoadedProperties($reflectionParameter);
+	}
+
+
+	public function testCreateFromReflectionConstant()
+	{
+		$tokenReflectionConstantMock = Mockery::mock('TokenReflection\IReflectionConstant', 'Nette\Object');
+		$reflectionConstant = $this->reflectionFactory->createFromReflection($tokenReflectionConstantMock);
+		$this->assertInstanceOf('ApiGen\Reflection\ReflectionConstant', $reflectionConstant);
+		$this->checkLoadedProperties($reflectionConstant);
+	}
+
+
+	private function checkLoadedProperties($object)
+	{
+		$this->assertInstanceOf(
+			'ApiGen\Configuration\Configuration',
+			PHPUnit_Framework_Assert::getObjectAttribute($object, 'configuration')
+		);
+
+		$this->assertInstanceOf(
+			'ApiGen\Parser\ParserResult',
+			PHPUnit_Framework_Assert::getObjectAttribute($object, 'parserResult')
+		);
+
+		$this->assertInstanceOf(
+			'ApiGen\Reflection\TokenReflection\ReflectionFactory',
+			PHPUnit_Framework_Assert::getObjectAttribute($object, 'reflectionFactory')
+		);
+	}
+
+}
