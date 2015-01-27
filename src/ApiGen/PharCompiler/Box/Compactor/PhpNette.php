7e7f40206cc9a1f02d6519a9f74f1b3d52911e4d
Tomas Votruba <info@tomasvotruba.cz>
2015-01-27 23:42:59 +0100

    PharCompiler: PhpNette compactor added

---
+++ src/ApiGen/PharCompiler/Box/Compactor/PhpNette.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * This file is part of the ApiGen (http://apigen.org)
+ *
+ * For the full copyright and license information, please view
+ * the file license.md that was distributed with this source code.
+ */
+
+namespace ApiGen\PharCompiler\Box\Compactor;
+
+use Herrera\Box\Compactor\CompactorInterface;
+
+
+/**
+ * Keeps Nette system annotations "method" and "return", minimizes PHP source and preserves line numbers.
+ */
+class PhpNette implements CompactorInterface
+{
+
+	/**
+	 * @var array
+	 */
+	protected $extensions = ['php'];
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function compact($contents)
+	{
+		$output = '';
+		foreach (token_get_all($contents) as $token) {
+			if (is_string($token)) {
+				$output .= $token;
+
+			} elseif ($token[0] === T_COMMENT) {
+				$output .= $this->preserveLineNumbers($token);
+
+			} elseif ($this->isCommentWithoutAnnotations($token, ['@return', '@method'])) {
+				$output .= $this->preserveLineNumbers($token);
+
+			} elseif ($token[0] === T_WHITESPACE) {
+				if (strpos($token[1], "\n") === FALSE) {
+					$output .= ' ';
+
+				} else {
+					$output .= $this->preserveLineNumbers($token);
+				}
+
+			} else {
+				$output .= $token[1];
+			}
+		}
+
+		return $output;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	private function preserveLineNumbers(array $token)
+	{
+		return str_repeat("\n", substr_count($token[1], "\n"));
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	private function isCommentWithoutAnnotations(array $token, array $annotationList)
+	{
+		if ($token[0] !== T_DOC_COMMENT) {
+			return FALSE;
+		}
+		foreach ($annotationList as $annotation) {
+			if (strpos($token[1], $annotation) !== FALSE) {
+				return FALSE;
+			}
+		}
+		return TRUE;
+	}
+
+
+	/**
+	 * Checks if the file is supported.
+	 *
+	 * @param string $file The file name.
+	 *
+	 * @return boolean TRUE if it is supported, FALSE if not.
+	 */
+	public function supports($file)
+	{
+		return (pathinfo($file, PATHINFO_EXTENSION) === 'php');
+	}
+
+}
---
+++ tests/PharCompiler/Box/Compactor/PhpNetteTest.php
@@ -0,0 +1,5 @@
+<?php
+
+class PhpNetteTest extends PHPUnit_Framework_TestCase
+{
+}
