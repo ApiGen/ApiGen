b407778f7dc25133bd9652af391c1734779b8110
Tomas Votruba <tomas.vot@gmail.com>
2015-03-07 13:23:21 +0100

    cleanup

--- travis/before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $TRAVIS_PHP_VERSION = '5.6' ]; then
-  PHPUNIT_FLAGS="--coverage-clover=coverage.clover"
-elseif [ $TRAVIS_PHP_VERSION != '7.0' ]
-  phpenv config-rm xdebug.ini
-fi
--- travis/before_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-vendor/bin/phpcs src tests --extensions=php --ignore=bootstrap,expected.php,source.php \
-	--standard=vendor/zenify/coding-standard/src/ZenifyCodingStandard/ruleset.xml -p
