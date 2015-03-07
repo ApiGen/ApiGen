6ba0fe8a5a6d5ba8acd1fc9fe0441f33874170be
Tomas Votruba <tomas.vot@gmail.com>
2015-03-07 17:59:04 +0100

    cs fixes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,6 @@ define('TEMP_DIR', $tempDir);
 @mkdir($tempDir, 0777, TRUE);
 
 
-register_shutdown_function(function() {
+register_shutdown_function(function () {
 	Nette\Utils\FileSystem::delete(__DIR__ . '/temp');
 });
