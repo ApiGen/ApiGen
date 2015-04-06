840ff5a136620bf679009d3e613eaf98aa3970c8
Tomas Votruba <tomas.vot@gmail.com>
2015-04-06 03:44:15 +0200

    license link fixed

--- src/Templating/Filters/Helpers/ElementUrlFactory.php
+++ src/Templating/Filters/Helpers/ElementUrlFactory.php
@@ -4,7 +4,7 @@
  * This file is part of the ApiGen (http://apigen.org)
  *
  * For the full copyright and license information, please view
- * the file license.md that was distributed with this source code.
+ * the file LICENSE that was distributed with this source code.
  */
 
 namespace ApiGen\Templating\Filters\Helpers;
