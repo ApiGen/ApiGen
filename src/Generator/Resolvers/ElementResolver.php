7d1097b7ad93830f2bea56add522b10764c6f3cd
Chris Burke <chris.burke@cakedc.com>
2015-10-07 22:21:54 -0500

    Fix CS

--- src/Generator/Resolvers/ElementResolver.php
+++ src/Generator/Resolvers/ElementResolver.php
@@ -152,7 +152,8 @@ class ElementResolver
 		}
 
 		if (($reflectionElement instanceof ClassReflectionInterface)
-			&& ($pos = strpos($definition, '::') || $pos = strpos($definition, '->'))) {
+			&& ($pos = strpos($definition, '::') || $pos = strpos($definition, '->'))
+		) {
 			$reflectionElement = $this->resolveContextForClassProperty($definition, $reflectionElement, $pos);
 			$definition = substr($definition, $pos + 2);
 
