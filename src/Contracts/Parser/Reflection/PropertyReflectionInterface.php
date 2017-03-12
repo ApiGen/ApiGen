5ca22a2ac7c23bbc32f4dc1e81a6ec4c5f461e21
TomasVotruba <tomas.vot@gmail.com>
2017-03-12 21:49:15 +0100

    drop file licenses, not really needed

--- src/Contracts/Parser/Reflection/PropertyReflectionInterface.php
+++ src/Contracts/Parser/Reflection/PropertyReflectionInterface.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * This file is part of the ApiGen (http://apigen.org)
- *
- * For the full copyright and license information, please view
- * the file LICENSE that was distributed with this source code.
- */
-
 namespace ApiGen\Contracts\Parser\Reflection;
 
 use ApiGen\Contracts\Parser\Reflection\Behavior\InClassInterface;
--- src/Utils/Neon/NeonFile.php
+++ src/Utils/Neon/NeonFile.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * This file is part of the ApiGen (http://apigen.org)
- *
- * For the full copyright and license information, please view
- * the file LICENSE that was distributed with this source code.
- */
-
 namespace ApiGen\Utils\Neon;
 
 use ApiGen\Utils\Neon\Exceptions\FileNotReadableException;
