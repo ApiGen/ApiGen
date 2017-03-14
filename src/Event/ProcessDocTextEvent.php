6365e94b83cf81877bdbbc0e16ade0a7537f02d5
TomasVotruba <tomas.vot@gmail.com>
2017-03-14 22:23:39 +0100

    add event for changing doc text [related to https://github.com/ApiGen/ApiGen/pull/795#issuecomment-286531166]

---
+++ src/Event/ProcessDocTextEvent.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace ApiGen\Event;
+
+use ApiGen\Contracts\Parser\Reflection\ElementReflectionInterface;
+use Symfony\Component\EventDispatcher\Event;
+
+class ProcessDocTextEvent extends Event
+{
+    /**
+     * @var string
+     */
+    private $text;
+
+    /**
+     * @var ElementReflectionInterface
+     */
+    private $reflectionElement;
+
+    public function __construct(string $text, ElementReflectionInterface $reflectionElement)
+    {
+        $this->text = $text;
+        $this->reflectionElement = $reflectionElement;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    public function getReflectionElement()
+    {
+        return $this->reflectionElement;
+    }
+
+    public function changeTest(string $text)
+    {
+        $this->text = $text;
+    }
+}
