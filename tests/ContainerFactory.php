be3d4272d9087c64375655f37b2b9ce1ac7f132a
TomasVotruba <tomas.vot@gmail.com>
2017-03-26 18:52:14 +0200

    coding style: spaces in neon

--- tests/ContainerFactory.php
+++ tests/ContainerFactory.php
@@ -5,14 +5,13 @@ namespace ApiGen\Tests;
 use Nette\Configurator;
 use Nette\DI\Container;
 
-class ContainerFactory
+final class ContainerFactory
 {
     public function create(): Container
     {
         $configurator = new Configurator;
         $configurator->setTempDirectory(TEMP_DIR);
         $configurator->addConfig(__DIR__ . '/config/default.neon');
-        $configurator->addParameters(['rootDir' => __DIR__ . '/..']);
         return $configurator->createContainer();
     }
 }
