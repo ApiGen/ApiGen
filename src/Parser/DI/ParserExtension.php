4f6687b4d22eb2e46c72da695e5cdb3f8fdce7a3
TomasVotruba <tomas.vot@gmail.com>
2017-03-31 15:33:53 +0200

    [ParserExtension] simplify

--- src/Parser/DI/ParserExtension.php
+++ src/Parser/DI/ParserExtension.php
@@ -2,31 +2,12 @@
 
 namespace ApiGen\Parser\DI;
 
-use ApiGen\Parser\Broker\Backend;
 use Nette\DI\Compiler;
 use Nette\DI\CompilerExtension;
-use TokenReflection\Broker;
 
 final class ParserExtension extends CompilerExtension
 {
     public function loadConfiguration(): void
-    {
-        $this->loadServicesFromConfig();
-
-        $containerBuilder = $this->getContainerBuilder();
-
-        $backend = $containerBuilder->addDefinition($this->prefix('backend'))
-            ->setClass(Backend::class);
-
-        $containerBuilder->addDefinition($this->prefix('broker'))
-            ->setClass(Broker::class)
-            ->setArguments([
-                $backend,
-                Broker::OPTION_DEFAULT & ~(Broker::OPTION_PARSE_FUNCTION_BODY | Broker::OPTION_SAVE_TOKEN_STREAM)
-            ]);
-    }
-
-    private function loadServicesFromConfig(): void
     {
         Compiler::loadDefinitions(
             $this->getContainerBuilder(),
