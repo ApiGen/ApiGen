d78ba14b8c2bbf110a14586126fb0965a4fd5b95
ek9 <dev@ek9.co>
2017-04-03 06:26:45 +0200

    ThemeConfigPathResolverTest make OS agnostic

--- tests/Theme/ThemeConfigPathResolverTest.php
+++ tests/Theme/ThemeConfigPathResolverTest.php
@@ -11,9 +11,10 @@ class ThemeConfigPathResolverTest extends PHPUnit_Framework_TestCase
 
     public function testResolve()
     {
-        $themeConfigPathResolver = new ThemeConfigPathResolver(__DIR__ . '/ThemeConfigPathResolverSource');
+        $themeConfigPathResolver =
+            new ThemeConfigPathResolver(__DIR__ . DIRECTORY_SEPARATOR . 'ThemeConfigPathResolverSource');
 
-        $configPath = $themeConfigPathResolver->resolve('/config.neon');
+        $configPath = $themeConfigPathResolver->resolve(DIRECTORY_SEPARATOR . 'config.neon');
         $this->assertSame(
             __DIR__  . DIRECTORY_SEPARATOR . 'ThemeConfigPathResolverSource' . DIRECTORY_SEPARATOR . 'config.neon',
             $configPath);
