0a3899ae6c7ec83a3d87d8558affcf027e5a7cd9
TomasVotruba <tomas.vot@gmail.com>
2017-05-11 14:16:22 +0200

    [Reflection] merge direct+indirect users, overkill to split and not supported by reflections

--- packages/Element/src/Tree/TraitUsersResolver.php
+++ packages/Element/src/Tree/TraitUsersResolver.php
@@ -2,7 +2,6 @@
 
 namespace ApiGen\Element\Tree;
 
-use ApiGen\Reflection\Contract\Reflection\Class_\ClassReflectionInterface;
 use ApiGen\Reflection\Contract\Reflection\Trait_\TraitReflectionInterface;
 use ApiGen\Reflection\Contract\ReflectionStorageInterface;
 
@@ -20,46 +19,16 @@ final class TraitUsersResolver
 
     public function getUsers(TraitReflectionInterface $traitReflection)
     {
-        return $this->getDirectUsers($traitReflection) + $this->getIndirectUsers($traitReflection);
-    }
-
-    /**
-     * @return ClassReflectionInterface[]
-     */
-    private function getDirectUsers(TraitReflectionInterface $traitReflection): array
-    {
-        $directUsers = [];
+        $users = [];
         foreach ($this->reflectionStorage->getClassReflections() as $classReflection) {
-            if (! in_array($traitReflection, $classReflection->getOwnTraits())) {
+            if (! in_array($traitReflection, $classReflection->getTraits())) {
                 continue;
             }
 
-            $directUsers[] = $classReflection;
+            $users[] = $classReflection;
         }
 
-        uksort($directUsers, 'strcasecmp');
-        return $directUsers;
-    }
-
-    /**
-     * @return ClassReflectionInterface[]
-     */
-    private function getIndirectUsers(TraitReflectionInterface $traitReflection): array
-    {
-        $indirectUsers = [];
-        foreach ($this->reflectionStorage->getClassReflections() as $classReflection) {
-            if (! $classReflection->usesTrait($traitReflection->getName())) {
-                continue;
-            }
-
-            if (! in_array($traitReflection, $classReflection->getOwnTraits())) {
-                continue;
-            }
-
-            $indirectUsers[] = $classReflection;
-        }
-
-        uksort($indirectUsers, 'strcasecmp');
-        return $indirectUsers;
+        uksort($users, 'strcasecmp');
+        return $users;
     }
 }
