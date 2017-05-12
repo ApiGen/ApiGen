61e0829b612041f4449bd1ae350e7502b9127836
TomasVotruba <tomas.vot@gmail.com>
2017-05-12 08:57:54 +0200

    [Reflection] add common test

---
+++ packages/Reflection/tests/Reflection/Partial/AccessLevelTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace ApiGen\Reflection\Tests\Reflection\Partial;
+
+use ApiGen\Reflection\Contract\Reflection\Partial\AccessLevelInterface;
+use ApiGen\Reflection\Tests\Reflection\Partial\Source\SomeClassWithAnnotations;
+use ApiGen\Tests\AbstractParserAwareTestCase;
+
+final class AccessLevelTest extends AbstractParserAwareTestCase
+{
+    /**
+     * @var AccessLevelInterface
+     */
+    private $reflection;
+
+    protected function setUp(): void
+    {
+        $this->parser->parseDirectories([__DIR__ . '/Source']);
+        $classReflection = $this->reflectionStorage->getClassReflections()[SomeClassWithAnnotations::class];
+        $this->reflection = $classReflection->getMethod('methodWithArgs');
+    }
+
+    public function test(): void
+    {
+        $this->assertTrue($this->reflection->isPublic());
+        $this->assertFalse($this->reflection->isProtected());
+        $this->assertFalse($this->reflection->isPrivate());
+    }
+}
---
+++ packages/Reflection/tests/Reflection/Partial/StartAndEndLineTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace ApiGen\Reflection\Tests\Reflection\Partial;
+
+use ApiGen\Reflection\Contract\Reflection\Partial\AnnotationsInterface;
+use ApiGen\Reflection\Contract\Reflection\Partial\StartAndEndLineInterface;
+use ApiGen\Reflection\Tests\Reflection\Partial\Source\SomeClassWithAnnotations;
+use ApiGen\Tests\AbstractParserAwareTestCase;
+
+final class StartAndEndLineTest extends AbstractParserAwareTestCase
+{
+    /**
+     * @var StartAndEndLineInterface
+     */
+    private $reflection;
+
+    protected function setUp(): void
+    {
+        $this->parser->parseDirectories([__DIR__ . '/Source']);
+        $this->reflection = $this->reflectionStorage->getClassReflections()[SomeClassWithAnnotations::class];
+    }
+
+    public function test(): void
+    {
+        $this->assertSame(12, $this->reflection->getStartLine());
+        $this->assertSame(24, $this->reflection->getEndLine());
+    }
+}
