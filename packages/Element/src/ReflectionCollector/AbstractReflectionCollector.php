40e049a95d7035bf417f87be1bf4f55d842feefe
TomasVotruba <tomas.vot@gmail.com>
2017-05-14 14:03:37 +0200

    cleanup

--- packages/Element/src/ReflectionCollector/AbstractReflectionCollector.php
+++ packages/Element/src/ReflectionCollector/AbstractReflectionCollector.php
@@ -2,19 +2,7 @@
 
 namespace ApiGen\Element\ReflectionCollector;
 
-use ApiGen\Contracts\Configuration\ConfigurationInterface;
 use ApiGen\Element\Contract\ReflectionCollector\ReflectionCollectorInterface;
-use ApiGen\Reflection\Contract\Reflection\Class_\ClassConstantReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Class_\ClassMethodReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Class_\ClassPropertyReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Class_\ClassReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Function_\FunctionReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Interface_\InterfaceConstantReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Interface_\InterfaceReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Partial\AnnotationsInterface;
-use ApiGen\Reflection\Contract\Reflection\Trait_\TraitMethodReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Trait_\TraitPropertyReflectionInterface;
-use ApiGen\Reflection\Contract\Reflection\Trait_\TraitReflectionInterface;
 
 abstract class AbstractReflectionCollector implements ReflectionCollectorInterface
 {
