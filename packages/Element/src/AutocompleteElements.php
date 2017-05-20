6f5a33909fa46cada27a21288082f9e4f1760908
TomasVotruba <tomas.vot@gmail.com>
2017-05-20 22:47:54 +0200

    [Element] and tests

--- packages/Element/src/AutocompleteElements.php
+++ packages/Element/src/AutocompleteElements.php
@@ -4,6 +4,7 @@ namespace ApiGen\Element;
 
 use ApiGen\Element\Contract\AutocompleteElementsInterface;
 use ApiGen\Reflection\Contract\ReflectionStorageInterface;
+use ApiGen\StringRouting\Route\ReflectionRoute;
 use ApiGen\StringRouting\Route\SourceCodeRoute;
 
 final class AutocompleteElements implements AutocompleteElementsInterface
@@ -14,14 +15,14 @@ final class AutocompleteElements implements AutocompleteElementsInterface
     private $reflectionStorage;
 
     /**
-     * @var SourceCodeRoute
+     * @var ReflectionRoute
      */
-    private $sourceCodeRoute;
+    private $reflectionRoute;
 
-    public function __construct(ReflectionStorageInterface $reflectionStorage, SourceCodeRoute $sourceCodeRoute)
+    public function __construct(ReflectionStorageInterface $reflectionStorage, ReflectionRoute $reflectionRoute)
     {
         $this->reflectionStorage = $reflectionStorage;
-        $this->sourceCodeRoute = $sourceCodeRoute;
+        $this->reflectionRoute = $reflectionRoute;
     }
 
     /**
@@ -31,22 +32,22 @@ final class AutocompleteElements implements AutocompleteElementsInterface
     {
         $elements = [];
         foreach ($this->reflectionStorage->getFunctionReflections() as $functionReflection) {
-            $path = $this->sourceCodeRoute->constructUrl($functionReflection);
+            $path = $this->reflectionRoute->constructUrl($functionReflection);
             $elements[$path] = $functionReflection->getName() . '()';
         }
 
         foreach ($this->reflectionStorage->getClassReflections() as $classReflection) {
-            $path = $this->sourceCodeRoute->constructUrl($classReflection);
+            $path = $this->reflectionRoute->constructUrl($classReflection);
             $elements[$path] = $classReflection->getName();
         }
 
         foreach ($this->reflectionStorage->getInterfaceReflections() as $interfaceReflection) {
-            $path = $this->sourceCodeRoute->constructUrl($interfaceReflection);
+            $path = $this->reflectionRoute->constructUrl($interfaceReflection);
             $elements[$path] = $interfaceReflection->getName();
         }
 
         foreach ($this->reflectionStorage->getTraitReflections() as $traitReflection) {
-            $path = $this->sourceCodeRoute->constructUrl($traitReflection);
+            $path = $this->reflectionRoute->constructUrl($traitReflection);
             $elements[$path] = $traitReflection->getName();
         }
 
