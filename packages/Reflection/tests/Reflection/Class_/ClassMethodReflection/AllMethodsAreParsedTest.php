a0ad43f3492a0e29518aeb8dbf89692fb929e01b
TomasVotruba <tomas.vot@gmail.com>
2017-06-01 20:23:34 +0200

    [Reflection] all methods are parsed [closes #848]

---
+++ packages/Reflection/tests/Reflection/Class_/ClassMethodReflection/AllMethodsAreParsedTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace ApiGen\Reflection\Tests\Reflection\Class_\ClassMethodReflection;
+
+use Api[R8Gen\Reflection\Tests\Reflection\Class_\ClassMethodReflection\Source\ClassWithManyMethods;
+use ApiGen\Tests\AbstractParserAwareTestCase;
+
+/**
+ * @link https://github.com/ApiGen/ApiGen/issues/848
+ */
+final class AllMethodsAreParsedTest extends AbstractParserAwareTestCase
+{
+    public function test(): void
+    {
+        $this->parser->parseDirectories([__DIR__ . '/Source']);
+
+        $classReflections = $this->reflectionStorage->getClassReflections();
+        $classReflection = $classReflections[ClassWithManyMethods::class];
+        $this->assertCount(12, $classReflection->getMethods());
+    }
+}
