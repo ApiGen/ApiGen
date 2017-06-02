93e520ebfa4409073024fe6c9ad4e1d3720d013b
TomasVotruba <tomas.vot@gmail.com>
2017-06-02 19:55:10 +0200

    details

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-define('TEMP_DIR', sys_get_temp_dir() . '/_apigen' . getmypid());
+define('TEMP_DIR', sys_get_temp_dir() . '/_apigen_cache');
 ApiGen\Utils\FileSystem::ensureDirectoryExists(TEMP_DIR);
 
 register_shutdown_function(function () {
