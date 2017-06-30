f2337f315e91a3b8a436ec1f9e8f51480c0d486b
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-06-30 17:55:38 +0200

    tests: added class extending from another project

---
+++ tests/Generator/ExtendingSources/ExtendingClass.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace ApiGen\Tests\Generator\ExtendingSources;
+
+use ApiGen\Tests\Generator\NotLoadedSources;
+
+class ExtendingClass extends NotLoadedSources\SomeClass
+{
+
+}
