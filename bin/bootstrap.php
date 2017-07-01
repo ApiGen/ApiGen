9019d33e56ae6baac2aad67db4179656bbcae937
Nelson Martell <nelson6e65@gmail.com>
2017-07-01 23:09:51 +0000

    :fire: Simplify constants on bootstrap script.
    :new: `APIGEN_IS_STANDALONE`.

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -2,31 +2,40 @@
 
 declare(strict_types=1);
 
+/**
+ * @var string ApiGen root directory.
+ * */
 define('APIGEN_CLI_ROOT_DIR', realpath(__DIR__.'/../'));
 
-// Cloned, downloaded or installed via `composer create-project`
-define('APIGEN_CLI_LOCAL_CONTEXT', 'local');
-// Installed via `composer [global] require`
-define('APIGEN_CLI_DIST_CONTEXT', 'distributed'); // Installed via composer require
+$standalone = null;
 
+// Check for autoloader in a local install (not installed via `composer [global] require`)
+$autoloader = realpath(APIGEN_CLI_ROOT_DIR.'/vendor/autoload.php');
 
-$local_autoloader = realpath(APIGEN_CLI_ROOT_DIR.'/vendor/autoload.php');
-$dist_autoloader  = realpath(APIGEN_CLI_ROOT_DIR.'/../../autoload.php');
-
-if ($local_autoloader) {
-    $cli_autoloader = $local_autoloader;
-    define('APIGEN_CLI_CONTEXT', APIGEN_CLI_LOCAL_CONTEXT);
-} elseif ($dist_autoloader) {
-    $cli_autoloader = $dist_autoloader;
-    define('APIGEN_CLI_CONTEXT', APIGEN_CLI_DIST_CONTEXT);
+if ($autoloader) {
+    // ApiGen is installed as clone, direct download or `composer create-project`.
+    // So, ApiGen is not a composer dependency.
+    $standalone = true;
 } else {
-    $cli_autoloader = null;
-    define('APIGEN_CLI_CONTEXT', null);
+    // Installed via `composer [global] require`.
+    $autoloader = realpath(APIGEN_CLI_ROOT_DIR.'/../../autoload.php');
+    
+    if ($autoloader) {
+        $standalone = false;
+    }
+}
 
+/**
+ * @var bool|null Indicates if runing ApiGen was installed as standalone. 
+ *   If is set to `null`, no autoloader was found.
+ * */
+define('APIGEN_IS_STANDALONE', $standalone);
+
+if (!$autoloader) {
     throw new RuntimeException(
         'Error! ApiGen was unable to find its autoloader. '.
         'Did you forget to run "composer update"?'
     );
 }
 
-require_once $cli_autoloader;
+require_once $autoloader;
