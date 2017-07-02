ad95bd33bb2869bed7f19061ab88d092389926a1
Nelson Martell <nelson6e65@gmail.com>
2017-07-02 09:08:59 -0400

    Improvements in binary autoloading (#921)
    * Move cli autoloading functionality to another file.
    
    Define some constants for root directory and runtime contexts.
    
    * Include 'bin' and 'boostrap' dirs for cs checks.
    
    'check-cs' and 'fix-cs' composer scripts will check for those dirs.
    
    * :fire: Move autoloader to 'bin/bootstrap.php'.
    
    * Improve error message.
    
    * :fire: Simplify constants on bootstrap script.
    
    :new: `APIGEN_IS_STANDALONE`.
    
    * :fire: Remove global constants from autoloader.
    
    * :memo: Minor improvements in 'bin/bootstrap.php'.

---
+++ bin/bootstrap.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+$rootDir = realpath(__DIR__.'/../');
+
+// Autoloader for standalone install (installed via `composer create-project` or cloned locally)
+$autoloader = realpath($rootDir.'/vendor/autoload.php');
+
+if (!$autoloader) {
+    // Installed via `composer [global] require`.
+    $autoloader = realpath($rootDir.'/../../autoload.php');
+}
+
+if (!$autoloader) {
+    throw new RuntimeException(
+        'ApiGen was unable to find its autoloader. '.
+        'Did you forget to run "composer update"?'
+    );
+}
+
+require_once $autoloader;
