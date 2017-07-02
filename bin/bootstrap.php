d4f5dbfb9cb78c910ae54d1798057b537ce0d34e
Nelson Martell <nelson6e65@gmail.com>
2017-07-02 06:03:12 +0000

    :fire: Remove global constants from autoloader.

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -2,34 +2,15 @@
 
 declare(strict_types=1);
 
-/**
- * @var string ApiGen root directory.
- * */
-define('APIGEN_CLI_ROOT_DIR', realpath(__DIR__.'/../'));
+$rootDir = realpath(__DIR__.'/../');
 
-$standalone = null;
+// Autoloader in a standalone install (not installed via `composer [global] require`)
+$autoloader = realpath($rootDir.'/vendor/autoload.php');
 
-// Check for autoloader in a local install (not installed via `composer [global] require`)
-$autoloader = realpath(APIGEN_CLI_ROOT_DIR.'/vendor/autoload.php');
-
-if ($autoloader) {
-    // ApiGen is installed as clone, direct download or `composer create-project`.
-    // So, ApiGen is not a composer dependency.
-    $standalone = true;
-} else {
+if (!$autoloader) {
     // Installed via `composer [global] require`.
-    $autoloader = realpath(APIGEN_CLI_ROOT_DIR.'/../../autoload.php');
-    
-    if ($autoloader) {
-        $standalone = false;
-    }
+    $autoloader = realpath($rootDir.'/../../autoload.php');
 }
-
-/**
- * @var bool|null Indicates if runing ApiGen was installed as standalone. 
- *   If is set to `null`, no autoloader was found.
- * */
-define('APIGEN_IS_STANDALONE', $standalone);
 
 if (!$autoloader) {
     throw new RuntimeException(
