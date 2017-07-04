6248ce507fe4f86f1f203f5526be431de2c03fa7
Vlasta Vesely <vlastimil.vesely7@email.cz>
2017-07-05 00:46:02 +0200

    fixed console progressbar (#927)
    * fixed console progressbar
    
    * added test for StepCounter

---
+++ tests/Console/Progress/StepCounterTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace ApiGen\Tests\Console\Helper;
+
+use ApiGen\Console\Progress\StepCounter;
+use ApiGen\Element\Namespace_\ParentEmptyNamespacesResolver;
+use ApiGen\Element\ReflectionCollector\NamespaceReflectionCollector;
+use ApiGen\Reflection\Parser\Parser;
+use ApiGen\Tests\AbstractParserAwareTestCase;
+
+final class StepCounterTest extends AbstractParserAwareTestCase
+{
+    public function testStepCount(): void
+    {
+        $this->parser->parseDirectories([__DIR__ . '/Source']);
+        $namespaceReflectionCollector = $this->container->get(NamespaceReflectionCollector::class);
+
+        $stepCounter = new StepCounter(
+            $this->reflectionStorage,
+            $namespaceReflectionCollector,
+            new ParentEmptyNamespacesResolver()
+        );
+
+        $count = 2;    // index.html + elementlist.js
+        $count += 5;   // classes.html + exceptions.html + interfaces.html + traits.html + functions.html
+        $count += 10;  // class, exception, interface, trait and function + their sources
+        $count ++;     // EmptyNamespace
+        $count ++;     // MyNamespace
+
+        $this->assertSame($count, $stepCounter->getStepCount());
+    }
+}
