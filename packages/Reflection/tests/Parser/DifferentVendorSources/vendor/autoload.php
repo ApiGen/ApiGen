6d8fd611f24c166ddd2e8dea28efc3d46b2a7466
Vlasta Vesely <vlastimil.vesely7@email.cz>
2017-07-07 11:38:48 +0200

    tests: added class extending from another project (#916)
    * tests: added class extending from another project
    
    * tests: DifferentVendorTest moved into Reflection/Parser
    
    * moved ExtendingClassTest

---
+++ packages/Reflection/tests/Parser/DifferentVendorSources/vendor/autoload.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+$loader = new Composer\Autoload\ClassLoader();
+
+// Files of the project that API is being generated for
+$loader->add('MyVendor', __DIR__ . '/../src');
+
+// Project's dependencies
+$loader->addClassMap([
+    'DifferentVendor\\DifferentClass' => __DIR__ . '/DifferentVendor/DifferentClass.php'
+]);
+
+$loader->register(true);
+
+return $loader;
