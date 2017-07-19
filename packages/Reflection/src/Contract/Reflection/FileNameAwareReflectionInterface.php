af6d596da4bfeaa84c4e06b6acbed1d533bb5b1b
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-07-19 14:30:57 +0200

    docs

--- packages/Reflection/src/Contract/Reflection/FileNameAwareReflectionInterface.php
+++ packages/Reflection/src/Contract/Reflection/FileNameAwareReflectionInterface.php
@@ -4,5 +4,8 @@ namespace ApiGen\Reflection\Contract\Reflection;
 
 interface FileNameAwareReflectionInterface
 {
+    /**
+     * @return string|null  returns null for PHP internal classes
+     */
     public function getFileName(): ?string;
 }
