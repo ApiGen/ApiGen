ea42fb862bd1ab4334d666a8879dad69f80886f0
TomasVotruba <tomas.vot@gmail.com>
2017-08-26 20:47:15 +0200

    [BetterRefleciton] use PhpParserLocator

--- packages/BetterReflection/src/SourceLocator/DirectorySourceLocatorFactory.php
+++ packages/BetterReflection/src/SourceLocator/DirectorySourceLocatorFactory.php
@@ -2,6 +2,7 @@
 
 namespace ApiGen\BetterReflection\SourceLocator;
 
+use Roave\BetterReflection\SourceLocator\Ast\Locator as AstLocator;
 use Roave\BetterReflection\SourceLocator\Type\AggregateSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\AutoloadSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\ComposerSourceLocator;
@@ -12,14 +13,24 @@ use Roave\BetterReflection\SourceLocator\Type\SourceLocator;
 final class DirectorySourceLocatorFactory
 {
     /**
+     * @var AstLocator
+     */
+    private $astLocator;
+
+    public function __construct(AstLocator $astLocator)
+    {
+        $this->astLocator = $astLocator;
+    }
+
+    /**
      * @param string[] $directories
      */
     public function createFromDirectories(array $directories): SourceLocator
     {
         $locators = [
-            new DirectoriesSourceLocator($directories),
-            new AutoloadSourceLocator,
-            new PhpInternalSourceLocator,
+            new DirectoriesSourceLocator($directories, $this->astLocator),
+            new AutoloadSourceLocator($this->astLocator),
+            new PhpInternalSourceLocator($this->astLocator),
         ];
 
         foreach ($directories as $directory) {
--- packages/BetterReflection/src/SourceLocator/FileSourceLocatorFactory.php
+++ packages/BetterReflection/src/SourceLocator/FileSourceLocatorFactory.php
@@ -2,6 +2,7 @@
 
 namespace ApiGen\BetterReflection\SourceLocator;
 
+use Roave\BetterReflection\SourceLocator\Ast\Locator as AstLocator;
 use Roave\BetterReflection\SourceLocator\Type\AggregateSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\AutoloadSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\PhpInternalSourceLocator;
@@ -11,17 +12,27 @@ use Roave\BetterReflection\SourceLocator\Type\SourceLocator;
 final class FileSourceLocatorFactory
 {
     /**
+     * @var AstLocator
+     */
+    private $astLocator;
+
+    public function __construct(AstLocator $astLocator)
+    {
+        $this->astLocator = $astLocator;
+    }
+
+    /**
      * @param string[] $files
      */
     public function createFromFiles(array $files): SourceLocator
     {
         $locators = [
-            new AutoloadSourceLocator,
-            new PhpInternalSourceLocator,
+            new AutoloadSourceLocator($this->astLocator),
+            new PhpInternalSourceLocator($this->astLocator),
         ];
 
         foreach ($files as $file) {
-            $locators[] = new SingleFileSourceLocator($file);
+            $locators[] = new SingleFileSourceLocator($file, $this->astLocator);
         }
 
         return new AggregateSourceLocator($locators);
