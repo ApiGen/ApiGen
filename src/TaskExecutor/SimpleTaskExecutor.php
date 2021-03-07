e5e5484ca73ec2e429c50f63040626006df3d70b
Jan Tvrdik <jan.tvrdik@gmail.com>
2021-03-07 18:04:18 +0100

    SimpleTaskExecutor: use loop

--- src/TaskExecutor/SimpleTaskExecutor.php
+++ src/TaskExecutor/SimpleTaskExecutor.php
@@ -2,18 +2,17 @@
 
 namespace ApiGenX\TaskExecutor;
 
+use React\EventLoop\LoopInterface;
 use React\Promise\Promise;
 use React\Promise\PromiseInterface;
 
 
 final class SimpleTaskExecutor implements TaskExecutor
 {
-	private TaskEnvironment $env;
-
-
-	public function __construct(TaskEnvironment $env)
-	{
-		$this->env = $env;
+	public function __construct(
+		private LoopInterface $loop,
+		private TaskEnvironment $env,
+	) {
 	}
 
 
@@ -26,7 +25,9 @@ final class SimpleTaskExecutor implements TaskExecutor
 	public function process(Task $task): PromiseInterface
 	{
 		return new Promise(function (callable $resolve) use ($task): void {
-			$resolve($task->run($this->env));
+			$this->loop->futureTick(function () use ($resolve, $task) {
+				$resolve($task->run($this->env));
+			});
 		});
 	}
 }
