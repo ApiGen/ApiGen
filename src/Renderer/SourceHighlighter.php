dfbeb4e5c0999e0434cb251798d868e4408663ab
Jan Tvrdik <jan.tvrdik@gmail.com>
2021-04-02 18:32:08 +0200

    SourceHighlighter: misc refactoring

--- src/Renderer/SourceHighlighter.php
+++ src/Renderer/SourceHighlighter.php
@@ -7,97 +7,103 @@ use PhpToken;
 
 final class SourceHighlighter
 {
+	public const PHP_TAG = 'php-tag';
+	public const PHP_KEYWORD = 'php-kw';
+	public const PHP_NUMBER = 'php-num';
+	public const PHP_STRING = 'php-str';
+	public const PHP_VARIABLE = 'php-var';
+	public const PHP_COMMENT = 'php-comment';
+
 	public array $tokenClass = [
-		T_OPEN_TAG => 'php-tag',
-		T_OPEN_TAG_WITH_ECHO => 'php-tag',
-		T_CLOSE_TAG => 'php-tag',
-		T_INCLUDE => 'php-kw',
-		T_INCLUDE_ONCE => 'php-kw',
-		T_REQUIRE => 'php-kw',
-		T_REQUIRE_ONCE => 'php-kw',
-		T_LOGICAL_OR => 'php-kw',
-		T_LOGICAL_XOR => 'php-kw',
-		T_LOGICAL_AND => 'php-kw',
-		T_PRINT => 'php-kw',
-		T_YIELD => 'php-kw',
-		T_YIELD_FROM => 'php-kw',
-		T_INSTANCEOF => 'php-kw',
-		T_NEW => 'php-kw',
-		T_CLONE => 'php-kw',
-		T_ELSEIF => 'php-kw',
-		T_ELSE => 'php-kw',
-		T_EVAL => 'php-kw',
-		T_EXIT => 'php-kw',
-		T_IF => 'php-kw',
-		T_ENDIF => 'php-kw',
-		T_ECHO => 'php-kw',
-		T_DO => 'php-kw',
-		T_WHILE => 'php-kw',
-		T_ENDWHILE => 'php-kw',
-		T_FOR => 'php-kw',
-		T_ENDFOR => 'php-kw',
-		T_FOREACH => 'php-kw',
-		T_ENDFOREACH => 'php-kw',
-		T_DECLARE => 'php-kw',
-		T_ENDDECLARE => 'php-kw',
-		T_AS => 'php-kw',
-		T_SWITCH => 'php-kw',
-		T_ENDSWITCH => 'php-kw',
-		T_CASE => 'php-kw',
-		T_DEFAULT => 'php-kw',
-		T_BREAK => 'php-kw',
-		T_CONTINUE => 'php-kw',
-		T_GOTO => 'php-kw',
-		T_FUNCTION => 'php-kw',
-		T_FN => 'php-kw',
-		T_CONST => 'php-kw',
-		T_RETURN => 'php-kw',
-		T_TRY => 'php-kw',
-		T_CATCH => 'php-kw',
-		T_FINALLY => 'php-kw',
-		T_THROW => 'php-kw',
-		T_USE => 'php-kw',
-		T_INSTEADOF => 'php-kw',
-		T_GLOBAL => 'php-kw',
-		T_STATIC => 'php-kw',
-		T_ABSTRACT => 'php-kw',
-		T_FINAL => 'php-kw',
-		T_PRIVATE => 'php-kw',
-		T_PROTECTED => 'php-kw',
-		T_PUBLIC => 'php-kw',
-		T_VAR => 'php-kw',
-		T_UNSET => 'php-kw',
-		T_ISSET => 'php-kw',
-		T_EMPTY => 'php-kw',
-		T_HALT_COMPILER => 'php-kw',
-		T_CLASS => 'php-kw',
-		T_TRAIT => 'php-kw',
-		T_INTERFACE => 'php-kw',
-		T_EXTENDS => 'php-kw',
-		T_IMPLEMENTS => 'php-kw',
-		T_LIST => 'php-kw',
-		T_ARRAY => 'php-kw',
-		T_NAMESPACE => 'php-kw',
-		T_LNUMBER => 'php-num',
-		T_DNUMBER => 'php-num',
-		T_CONSTANT_ENCAPSED_STRING => 'php-str',
-		T_ENCAPSED_AND_WHITESPACE => 'php-str',
-		T_VARIABLE => 'php-var',
-		T_COMMENT => 'php-comment',
-		T_DOC_COMMENT => 'php-comment',
+		T_OPEN_TAG => self::PHP_TAG,
+		T_OPEN_TAG_WITH_ECHO => self::PHP_TAG,
+		T_CLOSE_TAG => self::PHP_TAG,
+		T_INCLUDE => self::PHP_KEYWORD,
+		T_INCLUDE_ONCE => self::PHP_KEYWORD,
+		T_REQUIRE => self::PHP_KEYWORD,
+		T_REQUIRE_ONCE => self::PHP_KEYWORD,
+		T_LOGICAL_OR => self::PHP_KEYWORD,
+		T_LOGICAL_XOR => self::PHP_KEYWORD,
+		T_LOGICAL_AND => self::PHP_KEYWORD,
+		T_PRINT => self::PHP_KEYWORD,
+		T_YIELD => self::PHP_KEYWORD,
+		T_YIELD_FROM => self::PHP_KEYWORD,
+		T_INSTANCEOF => self::PHP_KEYWORD,
+		T_NEW => self::PHP_KEYWORD,
+		T_CLONE => self::PHP_KEYWORD,
+		T_ELSEIF => self::PHP_KEYWORD,
+		T_ELSE => self::PHP_KEYWORD,
+		T_EVAL => self::PHP_KEYWORD,
+		T_EXIT => self::PHP_KEYWORD,
+		T_IF => self::PHP_KEYWORD,
+		T_ENDIF => self::PHP_KEYWORD,
+		T_ECHO => self::PHP_KEYWORD,
+		T_DO => self::PHP_KEYWORD,
+		T_WHILE => self::PHP_KEYWORD,
+		T_ENDWHILE => self::PHP_KEYWORD,
+		T_FOR => self::PHP_KEYWORD,
+		T_ENDFOR => self::PHP_KEYWORD,
+		T_FOREACH => self::PHP_KEYWORD,
+		T_ENDFOREACH => self::PHP_KEYWORD,
+		T_DECLARE => self::PHP_KEYWORD,
+		T_ENDDECLARE => self::PHP_KEYWORD,
+		T_AS => self::PHP_KEYWORD,
+		T_SWITCH => self::PHP_KEYWORD,
+		T_ENDSWITCH => self::PHP_KEYWORD,
+		T_CASE => self::PHP_KEYWORD,
+		T_DEFAULT => self::PHP_KEYWORD,
+		T_BREAK => self::PHP_KEYWORD,
+		T_CONTINUE => self::PHP_KEYWORD,
+		T_GOTO => self::PHP_KEYWORD,
+		T_FUNCTION => self::PHP_KEYWORD,
+		T_FN => self::PHP_KEYWORD,
+		T_CONST => self::PHP_KEYWORD,
+		T_RETURN => self::PHP_KEYWORD,
+		T_CATCH => self::PHP_KEYWORD,
+		T_TRY => self::PHP_KEYWORD,
+		T_FINALLY => self::PHP_KEYWORD,
+		T_THROW => self::PHP_KEYWORD,
+		T_USE => self::PHP_KEYWORD,
+		T_INSTEADOF => self::PHP_KEYWORD,
+		T_GLOBAL => self::PHP_KEYWORD,
+		T_STATIC => self::PHP_KEYWORD,
+		T_ABSTRACT => self::PHP_KEYWORD,
+		T_FINAL => self::PHP_KEYWORD,
+		T_PRIVATE => self::PHP_KEYWORD,
+		T_PROTECTED => self::PHP_KEYWORD,
+		T_PUBLIC => self::PHP_KEYWORD,
+		T_VAR => self::PHP_KEYWORD,
+		T_UNSET => self::PHP_KEYWORD,
+		T_ISSET => self::PHP_KEYWORD,
+		T_EMPTY => self::PHP_KEYWORD,
+		T_HALT_COMPILER => self::PHP_KEYWORD,
+		T_CLASS => self::PHP_KEYWORD,
+		T_TRAIT => self::PHP_KEYWORD,
+		T_INTERFACE => self::PHP_KEYWORD,
+		T_EXTENDS => self::PHP_KEYWORD,
+		T_IMPLEMENTS => self::PHP_KEYWORD,
+		T_LIST => self::PHP_KEYWORD,
+		T_ARRAY => self::PHP_KEYWORD,
+		T_NAMESPACE => self::PHP_KEYWORD,
+		T_LNUMBER => self::PHP_NUMBER,
+		T_DNUMBER => self::PHP_NUMBER,
+		T_CONSTANT_ENCAPSED_STRING => self::PHP_STRING,
+		T_ENCAPSED_AND_WHITESPACE => self::PHP_STRING,
+		T_VARIABLE => self::PHP_VARIABLE,
+		T_COMMENT => self::PHP_COMMENT,
+		T_DOC_COMMENT => self::PHP_COMMENT,
 	];
 
 	public array $identifierClass = [
-		'true' => 'php-kw',
-		'false' => 'php-kw',
-		'null' => 'php-kw',
+		'true' => self::PHP_KEYWORD,
+		'false' => self::PHP_KEYWORD,
+		'null' => self::PHP_KEYWORD,
 	];
 
 
 	public function highlight(string $source): string
 	{
 		$align = strlen(strval(substr_count($source, "\n")));
-
 		$lineStart = "<div id=\"%1\$d\" class=\"source-line\"><a class=\"source-lineNum\" href=\"#%1\$d\">%1\${$align}d: </a>";
 		$lineEnd = '</div>';
 
@@ -111,7 +117,7 @@ final class SourceHighlighter
 			} else {
 				$html = htmlspecialchars($text);
 				$class = $this->tokenClass[$id] ?? ($id === T_STRING ? $this->identifierClass[strtolower($text)] ?? null : null);
-				$out .= $class ? sprintf('<span class="%s">%s</span>', $class, $html) : $html;
+				$out .= $class ? "<span class=\"{$class}\">{$html}</span>" : $html;
 			}
 		}
 
@@ -119,6 +125,9 @@ final class SourceHighlighter
 	}
 
 
+	/**
+	 * @return iterable<int, string>
+	 */
 	private function tokenize(string $source): iterable
 	{
 		foreach (PhpToken::tokenize($source, TOKEN_PARSE) as $token) {
