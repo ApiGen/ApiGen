a7eceeacd1aaab3fe7dd0d829eb92dcd5f627d92
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-06-01 14:09:23 +0200

    Indexer.indexNamespace: faster loop termination

--- src/Indexer.php
+++ src/Indexer.php
@@ -41,8 +41,12 @@ final class Indexer
 	public function indexNamespace(Index $index, string $namespace, string $namespaceLower, bool $primary): void
 	{
 		if (isset($index->namespace[$namespaceLower])) {
-			if ($primary && !$index->namespace[$namespaceLower]->primary) {
-				for ($info = $index->namespace[$namespaceLower]; $info->name->full !== ''; $info = $index->namespace[$info->name->namespaceLower]) {
+			if ($primary) {
+				for (
+					$info = $index->namespace[$namespaceLower];
+					!$info->primary && $info->name->full !== '';
+					$info = $index->namespace[$info->name->namespaceLower]
+				) {
 					$info->primary = true;
 				}
 			}
