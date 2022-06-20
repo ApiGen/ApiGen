2c718359f74a18c667c23c28c2197367eb46e33a
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-06-20 14:12:15 +0200

    Bootstrap: use correct tempDir

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -5,14 +5,19 @@ namespace ApiGenX;
 use ErrorException;
 use Nette\DI\Compiler;
 use Nette\DI\Config\Loader;
-use Nette\DI\Container;
 use Nette\DI\ContainerLoader;
 use Nette\DI\Extensions\ExtensionsExtension;
+use Nette\DI\Helpers as DIHelpers;
+use Nette\Schema\Helpers as SchemaHelpers;
 use Nette\Utils\FileSystem;
 use Symfony\Component\Console\Style\OutputStyle;
 
+use function array_map;
 use function dirname;
+use function getcwd;
 use function is_int;
+use function str_starts_with;
+use function sys_get_temp_dir;
 
 
 final class Bootstrap
@@ -30,30 +35,28 @@ final class Bootstrap
 	}
 
 
-	public static function createContainer(OutputStyle $output, string $tempDir, array $parameters, ?string $configPath): Container
+	public static function createApiGen(OutputStyle $output, array $parameters, array $configPaths): ApiGen
 	{
+		$currentWorkingDir = getcwd();
+		$systemTempDir = sys_get_temp_dir();
+
+		$config = self::mergeConfigs(
+			['parameters' => ['currentWorkingDir' => $currentWorkingDir, 'systemTempDir' => $systemTempDir]],
+			self::loadConfig(__DIR__ . '/../apigen.neon'),
+			...array_map(self::loadConfig(...), $configPaths),
+			...[['parameters' => self::resolvePaths($parameters, $currentWorkingDir)]],
+		);
+
+		$tempDir = DIHelpers::expand($config['parameters']['tempDir'], $config['parameters']);
 		$containerLoader = new ContainerLoader($tempDir, autoRebuild: true);
 
-		$containerGenerator = function (Compiler $compiler) use ($parameters, $configPath) {
+		$containerGenerator = function (Compiler $compiler) use ($config) {
 			$compiler->addExtension('extensions', new ExtensionsExtension);
-			$compiler->loadConfig(__DIR__ . '/../apigen.neon');
-
-			if ($configPath !== null) {
-				$compiler->loadConfig($configPath, new class extends Loader {
-					public function load(string $file, ?bool $merge = true): array {
-						$data = parent::load($file, $merge);
-						$data['parameters'] = Bootstrap::resolvePaths($data['parameters'] ?? [], dirname($file));
-
-						return $data;
-					}
-				});
-			}
-
-			$compiler->addConfig(['parameters' => $parameters]);
+			$compiler->addConfig($config);
 		};
 
 		$containerKey = [
-			$parameters,
+			$config,
 			PHP_VERSION_ID - PHP_RELEASE_VERSION,
 		];
 
@@ -63,11 +66,32 @@ final class Bootstrap
 		$container->addService('symfonyConsole.output', $output);
 		$container->initialize();
 
-		return $container;
+		return $container->getByType(ApiGen::class);
 	}
 
 
-	public static function resolvePaths(array $parameters, string $base): array
+	private static function mergeConfigs(array...$configs): array
+	{
+		$mergedConfig = [];
+
+		foreach ($configs as $config) {
+			$mergedConfig = SchemaHelpers::merge($config, $mergedConfig);
+		}
+
+		return $mergedConfig;
+	}
+
+
+	private static function loadConfig(string $path): array
+	{
+		$data = (new Loader)->load($path);
+		$data['parameters'] = self::resolvePaths($data['parameters'] ?? [], dirname($path));
+
+		return $data;
+	}
+
+
+	private static function resolvePaths(array $parameters, string $base): array
 	{
 		foreach (['tempDir', 'projectDir', 'outputDir'] as $parameterKey) {
 			if (isset($parameters[$parameterKey])) {
@@ -87,6 +111,8 @@ final class Bootstrap
 
 	private static function resolvePath(string $path, string $base): string
 	{
-		return FileSystem::isAbsolute($path) ? $path : FileSystem::joinPaths($base, $path);
+		return (FileSystem::isAbsolute($path) || str_starts_with($path, '%'))
+			? $path
+			: FileSystem::joinPaths($base, $path);
 	}
 }
