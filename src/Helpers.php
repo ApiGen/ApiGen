a3c37accde4f35203e4bea294f69519c3160b331
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-06-23 13:01:39 +0200

    more reliable baseDir resolution

--- src/Helpers.php
+++ src/Helpers.php
@@ -4,11 +4,40 @@ namespace ApiGenX;
 
 use ReflectionClass;
 
+use function array_shift;
 use function realpath;
+use function strlen;
+use function substr;
+
+use const DIRECTORY_SEPARATOR;
 
 
 final class Helpers
 {
+	/**
+	 * @param string[] $paths indexed by [], non-empty
+	 */
+	public static function baseDir(array $paths): string
+	{
+		$paths = array_map(self::realPath(...), $paths);
+		$first = array_shift($paths);
+		$j = 0;
+
+		for ($i = 0; $i < strlen($first); $i++) {
+			foreach ($paths as $path) {
+				if (!isset($path[$i]) || $path[$i] !== $first[$i]) {
+					return substr($first, 0, $j);
+
+				} elseif ($first[$i] === DIRECTORY_SEPARATOR) {
+					$j = $i;
+				}
+			}
+		}
+
+		return $first;
+	}
+
+
 	public static function realPath(string $path): string
 	{
 		$realPath = realpath($path);
