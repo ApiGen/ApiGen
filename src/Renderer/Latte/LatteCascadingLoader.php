28ff91bb82f1f58219354194d7ba01768e8ec230
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-06-26 09:21:54 +0200

    fix LatteCascadingLoader

--- src/Renderer/Latte/LatteCascadingLoader.php
+++ src/Renderer/Latte/LatteCascadingLoader.php
@@ -7,14 +7,15 @@ use Latte;
 use function is_file;
 
 
-class LatteCascadingLoader extends Latte\Loaders\FileLoader
+class LatteCascadingLoader implements Latte\Loader
 {
 	/**
 	 * @param string[] $baseDirs
 	 */
-	public function __construct(protected array $baseDirs)
-	{
-		parent::__construct();
+	public function __construct(
+		protected array $baseDirs,
+		protected Latte\Loader $inner = new Latte\Loaders\FileLoader(),
+	) {
 	}
 
 
@@ -24,7 +25,7 @@ class LatteCascadingLoader extends Latte\Loaders\FileLoader
 			$path = $baseDir . '/' . $name;
 
 			if (is_file($path)) {
-				return parent::getContent($path);
+				return $this->inner->getContent($path);
 			}
 		}
 
@@ -38,11 +39,17 @@ class LatteCascadingLoader extends Latte\Loaders\FileLoader
 			$path = $baseDir . '/' . $name;
 
 			if (is_file($path)) {
-				return parent::isExpired($path, $time);
+				return $this->inner->isExpired($path, $time);
 			}
 		}
 
 		throw new Latte\RuntimeException("Missing template file '$name'.");
+	}
+
+
+	public function getReferredName(string $name, string $referringName): string
+	{
+		return $this->inner->getReferredName($name, $referringName);
 	}
 
 
@@ -52,7 +59,7 @@ class LatteCascadingLoader extends Latte\Loaders\FileLoader
 			$path = $baseDir . '/' . $name;
 
 			if (is_file($path)) {
-				return parent::getUniqueId($path);
+				return $this->inner->getUniqueId($path);
 			}
 		}
 
