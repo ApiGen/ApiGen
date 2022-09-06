9df44a05033d485256298d501e58c193222e4968
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-09-06 22:23:19 +0200

    add test for constant in traits

---
+++ tests/Features/Php82/TraitConstants/TraitConstants.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace ApiGenTests\Features\Php82\TraitConstants;
+
+
+trait TraitConstants
+{
+	public const A = 1;
+	protected const B = 2;
+	private const C = 2;
+}
