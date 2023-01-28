cb78ca82e7750d2f02d4dba551aa5c76fda6b922
Jan Tvrdík <git@jantvrdik.com>
2023-01-28 21:20:01 +0100

    update tests structure

--- tests/Features/PhpDoc/Alias/LocalTypeAlias.php
+++ tests/Features/PhpDoc/Alias/LocalTypeAlias.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace ApiGenTests\Features\Alias;
+namespace ApiGenTests\Features\PhpDoc\Alias;
 
 use DateTimeImmutable;
 
--- tests/Features/PhpDoc/Mixin/Mixin.php
+++ tests/Features/PhpDoc/Mixin/Mixin.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace ApiGenTests\Features\Mixin;
+namespace ApiGenTests\Features\PhpDoc\Mixin;
 
 /**
  * @method string getFoo()
