1c13561d8d3f0ca59971247962c7d08f38b01c80
Jan Tvrdík <git@jantvrdik.com>
2023-01-30 11:58:47 +0100

    StringExprInfo: tweak encoding algorithm

--- src/Info/Expr/StringExprInfo.php
+++ src/Info/Expr/StringExprInfo.php
@@ -4,24 +4,19 @@ namespace ApiGen\Info\Expr;
 
 use ApiGen\Info\ExprInfo;
 
-use function dechex;
-use function ltrim;
 use function mb_ord;
 use function ord;
 use function preg_match;
 use function preg_replace_callback;
-use function str_pad;
+use function sprintf;
 use function strlen;
-use function strtoupper;
-
-use const STR_PAD_LEFT;
 
 
 class StringExprInfo implements ExprInfo
 {
 	public function __construct(
 		public string $value,
-		public ?string $raw,
+		public ?string $raw = null,
 	) {
 	}
 
@@ -29,8 +24,8 @@ class StringExprInfo implements ExprInfo
 	public function toString(): string
 	{
 		$utf8 = (bool) preg_match('##u', $this->value);
-		$pattern = $utf8 ? '#[\p{C}\\\\]#u' : '#[\x00-\x1F\x7F-\xFF\\\\]#';
-		$special = ["\r" => '\r', "\n" => '\n', "\t" => '\t', '\\' => '\\\\'];
+		$pattern = $utf8 ? '#[\p{C}"\'\\\\]#u' : '#[\x00-\x1F\x7F-\xFF"\'\\\\]#';
+		$special = ["\r" => '\r', "\n" => '\n', "\t" => '\t', '\\' => '\\\\', '"' => '\\"', '\'' => '\''];
 
 		$s = preg_replace_callback(
 			$pattern,
@@ -39,15 +34,18 @@ class StringExprInfo implements ExprInfo
 					return $special[$m[0]];
 
 				} elseif (strlen($m[0]) === 1) {
-					return '\x' . str_pad(strtoupper(dechex(ord($m[0]))), 2, '0', STR_PAD_LEFT);
+					return sprintf('\x%02X', ord($m[0]));
 
 				} else {
-					return '\u{' . strtoupper(ltrim(dechex(mb_ord($m[0])), '0')) . '}';
+					return sprintf('\u{%X}', mb_ord($m[0]));
 				}
 			},
 			$this->value,
+			-1,
+			$count,
 		);
 
-		return '"' . $s . '"';
+		$quote = $count === 0 ? "'" : '"';
+		return $quote . $s . $quote;
 	}
 }
---
+++ tests/StringExprInfoTest.phpt
@@ -0,0 +1,44 @@
+<?php declare(strict_types = 1);
+
+namespace ApiGenTests;
+
+use ApiGen\Info\Expr\StringExprInfo;
+use Tester\Assert;
+use Tester\Environment;
+use Tester\TestCase;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+/**
+ * @testCase
+ */
+class StringExprInfoTest extends TestCase
+{
+	/**
+	 * @dataProvider provideToStringData
+	 */
+	public function testToString(string $value, string $expected): void
+	{
+		$actual = (new StringExprInfo($value))->toString();
+		Assert::same($expected, $actual);
+	}
+
+
+	public function provideToStringData(): iterable
+	{
+		yield ['foo', "'foo'"];
+		yield ['foo\\bar', '"foo\\\\bar"'];
+		yield ['foo\'bar', '"foo\'bar"'];
+		yield ['foo"bar', '"foo\\"bar"'];
+		yield ["foo\nbar", '"foo\\nbar"'];
+		yield ["foo\r\n\tbar", '"foo\\r\\n\\tbar"'];
+		yield ["foo\x00bar", '"foo\\x00bar"'];
+		yield ["foo\u{0200B}bar", '"foo\\u{200B}bar"'];
+		yield ["foo\x00\u{0200B}bar", '"foo\\x00\\u{200B}bar"'];
+	}
+}
+
+
+Environment::setup();
+(new StringExprInfoTest)->run();
