ea943744d52388a9044313d1ad087d5f5be4b36e
Jan Tvrdík <git@jantvrdik.com>
2023-02-01 18:23:12 +0100

    add tests for PHP 5.0 features

---
+++ tests/Features/Php50/FullObjectModel/MyClassParent.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace ApiGenTests\Features\Php50\FullObjectModel;
+
+
+abstract class MyClassParent
+{
+	/** @var int */
+	private $int;
+
+	/** @var string */
+	protected $string;
+
+
+	/**
+	 * @param int    $int
+	 * @param string $string
+	 */
+	public function __construct($int, $string)
+	{
+		$this->int = $int;
+		$this->string = $string;
+	}
+
+
+	/**
+	 * @return int
+	 */
+	abstract protected function getNumber();
+}
